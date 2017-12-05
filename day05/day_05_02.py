61d3f104193033c07f591b3d8b90c9dbc3f906f8
Matthieu Totet <matthieu.totet@stylight.com>
2017-12-05 08:39:36 +0100

    Day05, meh version

---
+++ day05/day_05_02.py
@@ -0,0 +1,20 @@
+import sys
+
+DATA = [int(l) for l in sys.stdin.readlines()]
+print(DATA)
+TEST = [0,3,0,1,-3]
+
+idx = 0
+cpt = 0
+while idx <= len(DATA):
+    jump =  DATA[idx]
+    if jump >= 3:
+        DATA[idx] -=1
+    else:
+        DATA[idx] +=1
+    idx = idx + jump
+    cpt+=1
+    if idx >= len(DATA):
+        break
+
+print("== %s =="%cpt)
