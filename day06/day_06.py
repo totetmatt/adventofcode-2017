255028ef9771f3f39b12e88d5cda4340ea755331
Matthieu Totet <matthieu.totet@stylight.com>
2017-12-06 17:34:53 +0100

    Much better

--- day06/day_06.py
+++ day06/day_06.py
@@ -1,33 +1,21 @@
 import sys
-
+import collections
 states = {}
 cpt=0
 
-TEST = [0,2,7,0]
-
-TEST =[int(v) for v in sys.stdin.read().split('\t')]
-size = len(TEST)
-print(TEST)
-# print(TEST.index(max(TEST)))
-states[str(TEST)] = cpt
+DATA = [0, 2, 7, 0]
+DATA =[int(v) for v in sys.stdin.read().split('\t')]
 while True:
-
-    a = max(TEST)
-    idx = TEST.index(a)
-
-    TEST[idx]=0
-    curr_idx = (idx +1)  % size
-    for _ in range(0,a):
-        TEST[curr_idx] += 1
-        curr_idx = (curr_idx + 1) % size
-
-    cpt +=1
-    if str(TEST) in states :
+    print(DATA)
+    a = max(DATA)
+    idx = DATA.index(a)
+    size = len(DATA)
+    distrib =  collections.Counter((idx + r)%size for r in range(1,a+1))
+    DATA[idx] = 0
+    DATA = [distrib.get(i, 0) + v for i, v in enumerate(DATA)]
+    cpt+=1
+    if str(DATA) in states:
         break
+    states[str(DATA)] = cpt
 
-    states[str(TEST)] = cpt
-
-
-print(cpt,cpt - states[str(TEST)])
-
-
+print(cpt, cpt - states[str(DATA)])
