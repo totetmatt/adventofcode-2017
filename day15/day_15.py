a4e5f9ea691cc3168c6d18f95442c3db3a93b1cf
Matthieu Totet <matthieu.totet@stylight.com>
2017-12-15 08:43:34 +0100

    More functional solution :D

--- day15/day_15.py
+++ day15/day_15.py
@@ -4,12 +4,12 @@ def next(current,factor,constraint,divide=2147483647):
         if current % constraint == 0:
             return current
 
+def gen(start,factor,constraint,size):
+    while size >0:
+        start = next(start,factor,constraint)
+        yield start
+        size -=1
 
-
-"""
-Generator A starts with 277
-Generator B starts with 349
-"""
 A= 277
 A_factor = 16807
 A_constraint=4
@@ -17,16 +17,8 @@ A_constraint=4
 B=349
 B_factor = 48271
 B_constraint = 8
-cpt = 0
 
+def run(nb_iteration=5000000):
+    return sum([1 for z in zip(gen(A,A_factor,A_constraint,5000000),gen(B,B_factor,B_constraint,5000000)) if format(z[0],'#032b')[-16:] == format(z[1],'#032b')[-16:] ])
 
-for r in range(0,5000000):
-    # print(format(A,'#032b')[-16:],A)
-    # print(format(B,'#032b')[-16:],B)
-    if format(A,'#032b')[-16:] == format(B,'#032b')[-16:]:
-        cpt +=1
-    # print('----')
-    A = next(A,A_factor,A_constraint)
-    B = next(B,B_factor,B_constraint)
-
-print(cpt)
+print(run())
